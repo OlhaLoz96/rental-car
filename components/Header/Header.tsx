b3c44f2b3f1e39d281a26d4054cdbf993a08635c
Olha Loz <olyakarabyn@gmail.com>
2026-03-11 12:44:46 +0200

    implement header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,29 @@
+import css from './Header.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className={css.header}>
+      <div className={`container ${css.headerContainer}`}>
+        <Link href="/" aria-label="Home" className={css.logo}>
+          <Image src="/logo.svg" width={104} height={16} alt="logo" />
+        </Link>
+        <nav aria-label="Main Navigation">
+          <ul className={css.navigation}>
+            <li>
+              <Link href="/" className={css.navigationLink}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/catalog" className={css.navigationLink}>
+                Catalog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
